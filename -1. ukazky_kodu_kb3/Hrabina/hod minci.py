a558d97f276798be867fd4ed28af2681c63e65cd
Filip cetti <st025470@student.cichnovabrno.cz>
2025-10-06 11:33:10 +0200

    hod minci

---
+++ -1. ukazky_kodu_kb3/Hrabina/hod minci.py
@@ -0,0 +1,17 @@
+import random
+
+
+def hod(vysledek,vyber):
+    vyber = int(input("vyber panna nebo orel"))
+    vysledek = 
+    if vyber == panna:
+        random.choice(vysledek)
+        if random.choice == panna:
+            zustatek =+ 50
+        else:
+            zustatek =- 50
+    return zustatek
+
+hod()
+zustatek = 100
+print (zustatek)
